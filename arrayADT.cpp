dfbdf7dc264c537386c5df9a0549197794885788
Sushant2 <er.sushant.2@gmail.com>
2020-05-07 00:35:28 +0530

    Changes c codes to c++

--- arrayADT.cpp
+++ arrayADT.cpp
@@ -1,177 +1,356 @@
 #include <iostream>
 using namespace std;
-struct Array
+class Array
 {
+private:
     int *A;
     int size;
     int length;
+    void swap(int *x, int *y)
+    {
+        int temp;
+        temp = *x;
+        *x = *y;
+        *y = temp;
+    }
+
+public:
+    Array()
+    {
+        size = 10;
+        length = 0;
+        A = new int[size];
+    }
+    Array(int sz)
+    {
+        size = sz;
+        length = 0;
+        A = new int[size];
+    }
+    ~Array()
+    {
+        delete[] A;
+    }
+    void Display()
+    {
+        int i;
+        cout << "Elements are:";
+        for (i = 0; i < length; i++)
+            cout << A[i] << " ";
+    }
+    void Append(int x)
+    {
+        if (length < size)
+            A[length++] = x;
+    }
+    void Insert(int index, int x)
+    {
+        int i;
+        if (index >= 0 && index <= length)
+        {
+            for (i = length; i > index; i--)
+                A[i] = A[i - 1];
+            A[index] = x;
+            length++;
+        }
+    }
+    int Delete(int index)
+    {
+        int x = 0;
+        int i;
+        if (index >= 0 && index < length)
+        {
+            x = A[index];
+            for (i = index; i < length - 1; i++)
+                A[i] = A[i + 1];
+            length--;
+            return x;
+        }
+        return 0;
+    }
+    int LinearSearch(int key)
+    {
+        int i;
+        for (i = 0; i < length; i++)
+        {
+            if (key == A[i])
+            {
+                swap(&A[i], &A[0]);
+                return i;
+            }
+        }
+        return -1;
+    }
+    int BinarySearch(int key)
+    {
+        int l, mid, h;
+        l = 0;
+        h = length - 1;
+        while (l <= h)
+        {
+            mid = (l + h) / 2;
+            if (key == A[mid])
+                return mid;
+            else if (key < A[mid])
+                h = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+    int RBinSearch(int a[], int l, int h, int key)
+    {
+        int mid;
+        if (l <= h)
+        {
+            mid = (l + h) / 2;
+            if (key == a[mid])
+                return mid;
+            else if (key < a[mid])
+                return RBinSearch(a, l, mid - 1, key);
+            else
+                return RBinSearch(a, mid + 1, h, key);
+        }
+        return -1;
+    }
+    int Get(int index)
+    {
+        if (index >= 0 && index < length)
+            return A[index];
+        return -1;
+    }
+    void Set(int index, int x)
+    {
+        if (index >= 0 && index < length)
+            A[index] = x;
+    }
+    int Max()
+    {
+        int max = A[0];
+        int i;
+        for (i = 1; i < length; i++)
+        {
+            if (A[i] > max)
+                max = A[i];
+        }
+        return max;
+    }
+    int Min()
+    {
+        int min = A[0];
+        int i;
+        for (i = 1; i < length; i++)
+        {
+            if (A[i] < min)
+                min = A[i];
+        }
+        return min;
+    }
+    int Sum()
+    {
+        int s = 0;
+        int i;
+        for (i = 0; i < length; i++)
+            s += A[i];
+        return s;
+    }
+    float Avg()
+    {
+        return (float)Sum() / length;
+    }
+    void Reverse()
+    {
+        int *B;
+        int i, j;
+        B = new int[length];
+        for (i = length - 1, j = 0; i >= 0; i--, j++)
+            B[j] = A[i];
+        for (i = 0; i < length; i++)
+            A[i] = B[i];
+    }
+    void Reverse2()
+    {
+        int i, j;
+        for (i = 0, j = length - 1; i < j; i++, j--)
+        {
+            swap(&A[i], &A[j]);
+        }
+    }
+    void InsertSort(int x)
+    {
+        int i = length - 1;
+        if (length == size)
+            return;
+        while (i >= 0 && A[i] > x)
+        {
+            A[i + 1] = A[i];
+            i--;
+        }
+        A[i + 1] = x;
+        length++;
+    }
+    int isSorted()
+    {
+        int i;
+        for (i = 0; i < length - 1; i++)
+        {
+            if (A[i] > A[i + 1])
+                return 0;
+        }
+        return 1;
+    }
+    void Rearrange()
+    {
+        int i, j;
+        i = 0;
+        j = length - 1;
+        while (i < j)
+        {
+            while (A[i] < 0)
+                i++;
+            while (A[j] >= 0)
+                j--;
+            if (i < j)
+                swap(&A[i], &A[j]);
+        }
+    }
+    Array *Merge(Array arr2)
+    {
+        int i, j, k;
+        i = j = k = 0;
+        Array *arr3 = new Array(length + arr2.length);
+
+        while (i < length && j < arr2.length)
+        {
+            if (A[i] < arr2.A[j])
+                arr3->A[k++] = A[i++];
+            else
+                arr3->A[k++] = arr2.A[j++];
+        }
+        for (; i < length; i++)
+            arr3->A[k++] = A[i];
+        for (; j < arr2.length; j++)
+            arr3->A[k++] = arr2.A[j];
+        arr3->length = length + arr2.length;
+        arr3->size = 10;
+        return arr3;
+    }
+    Array *Union(Array arr2)
+    {
+        int i, j, k;
+        i = j = k = 0;
+        Array *arr3 = new Array(length + arr2.length);
+        while (i < length && j < arr2.length)
+        {
+            if (A[i] < arr2.A[j])
+                arr3->A[k++] = A[i++];
+            else if (arr2.A[j] < A[i])
+                arr3->A[k++] = arr2.A[j++];
+            else
+            {
+                arr3->A[k++] = A[i++];
+                j++;
+            }
+        }
+        for (; i < length; i++)
+            arr3->A[k++] = A[i];
+        for (; j < arr2.length; j++)
+            arr3->A[k++] = arr2.A[j];
+        arr3->length = k;
+        arr3->size = 10;
+        return arr3;
+    }
+    Array *Intersection(Array arr2)
+    {
+        int i, j, k;
+        i = j = k = 0;
+        Array *arr3 = new Array(length + arr2.length);
+        while (i < length && j < arr2.length)
+        {
+            if (A[i] < arr2.A[j])
+                i++;
+            else if (arr2.A[j] < A[i])
+                j++;
+            else if (A[i] == arr2.A[j])
+            {
+                arr3->A[k++] = A[i++];
+                j++;
+            }
+        }
+        arr3->length = k;
+        arr3->size = 10;
+        return arr3;
+    }
+    Array *Difference(Array arr2)
+    {
+        int i, j, k;
+        i = j = k = 0;
+        Array *arr3 = new Array(length + arr2.length);
+        while (i < length && j < arr2.length)
+        {
+            if (A[i] < arr2.A[j])
+                arr3->A[k++] = A[i++];
+            else if (arr2.A[j] < A[i])
+                j++;
+            else
+            {
+                i++;
+                j++;
+            }
+        }
+        for (; i < length; i++)
+            arr3->A[k++] = A[i];
+        arr3->length = k;
+        arr3->size = 10;
+        return arr3;
+    }
 };
-void swap(int *x, int *y)
-{
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
-void Display(struct Array arr)
-{
-    int i;
-    cout << "\nElements are\n";
-    for (i = 0; i < arr.length; i++)
-        cout << arr.A[i] << " ";
-}
-int get(struct Array arr, int idx)
-{
-    if (idx >= 0 && idx < arr.length)
-        return arr.A[idx];
-}
-int set(struct Array arr, int i, int val)
-{
-    if (i >= 0 && i < arr.length)
-        arr.A[i] = val;
-}
-int max(struct Array arr)
-{
-    int maxV = arr.A[0];
-    for (int i = 0; i < arr.length; i++)
-        if (arr.A[i] > maxV)
-            maxV = arr.A[i];
-    return maxV;
-}
-int min(struct Array arr)
-{
-    int minV = arr.A[0];
-    for (int i = 0; i < arr.length; i++)
-        if (arr.A[i] < minV)
-            minV = arr.A[i];
-    return minV;
-}
-int sum(struct Array arr)
-{
-    int total = 0;
-    for (int i = 0; i < arr.length; i++)
-        total = total + arr.A[i];
-    return total;
-}
-// using recursion
-// int sumR(struct Array arr, int n){
-//     if(n<=0)
-//         return 0;
-//     return sumR(arr.A, n-1) + arr.A[arr.length];
-// }
-float Avg(struct Array arr)
-{
-    return (float)sum(arr) / arr.length;
-}
-void Reverse(struct Array *arr)
-{
-    int *B, i, j;
-    B = new int[arr->length];
-    for (i = arr->length - 1, j = 0; i >= 0; i--, j++)
-        B[j] = arr->A[i];
-    for (i = 0; i < arr->length; i++)
-        arr->A[i] = B[i];
-}
-void Reverse2(struct Array *arr)
-{
-    int i, j;
-    for (i = 0, j = arr->length - 1; i < j; i++, j--)
-        swap(&arr->A[i], &arr->A[j]);
-}
-void insertsort(struct Array *arr, int x)
-{
-    int i = arr->length - 1;
-    if (arr->length == arr->size)
-        return;
-    while (i >= 0 && arr->A[i] > x)
-    {
-        arr->A[i + 1] = arr->A[i];
-        i--;
-    }
-    arr->A[i + 1] = x;
-    arr->length++;
-}
-bool isSorted(struct Array arr)
-{
-    int i;
-    for (i = 0; i < arr.length - 1; i++)
-        if (arr.A[i] > arr.A[i + 1])
-            return false;
-    return true;
-}
-void rearrange(struct Array *arr)
-{
-    int i, j;
-    i = 0;
-    j = arr->length;
-
-    while (i < j)
-    {
-        while (arr->A[i] < 0)
-            i++;
-        while (arr->A[j] >= 0)
-            j--;
-        if (i < j)
-            swap(&arr->A[i], &arr->A[j]);
-    }
-}
-struct Array *merge(struct Array *arr1, struct Array *arr2)
-{
-    int i, j, k;
-    i = j = k = 0;
-    struct Array *arr3 = (struct Array *)malloc(sizeof(struct Array));
-
-    while (i < arr1->length && j < arr2->length)
-    {
-        if (arr1->A[i] < arr2->A[j])
-            arr3->A[k++] = arr1->A[i++];
-        else
-            arr3->A[k++] = arr2->A[j++];
-    }
-    for (; i < arr1->length; i++)
-        arr3->A[k++] = arr1->A[i];
-    for (; j < arr2->length; j++)
-        arr3->A[k++] = arr2->A[j];
-    arr3->length = arr1->length + arr2->length;
-    arr3->size = 10;
-    return arr3;
-}
 int main()
 {
-    struct Array arr1, arr2, *arr3;
-    int n1, n2;
-    cout << "Enter size of an array:";
-    cin >> arr1.size;
-    cout << "Enter size of an 2nd array:";
-    cin >> arr2.size;
-    arr1.A = new int[arr1.size];
-    arr2.A = new int[arr2.size];
-    arr1.length = 0;
-    arr2.length = 0;
-    cout << "Enter how many numbers for 1st array:";
-    cin >> n1;
-    cout << "Enter how many numbers for 2st array:";
-    cin >> n2;
-    cout << "Enter Elements for 1st array:\n";
-    for (int i = 0; i < n1; i++)
-        cin >> arr1.A[i];
-    arr1.length = n1;
-    cout << "Enter elements for 2nd array:";
-    for (int i = 0; i < n2; i++)
-        cin >> arr2.A[i];
-    arr2.length = n2;
-    // cout<<get(arr, 4);
-    // cout<<set(arr, 3, 75);
-    // cout<<max(arr);
-    // cout<<min(arr);
-    // cout<<sumR(arr, arr.length);
-    // cout<<Avg(arr);
-    // Reverse(&arr);
-    // Reverse2(&arr);
-    // insertsort(&arr, 4);
-    // cout<<isSorted(arr);
-    // rearrange(&arr);
-    arr3 = merge(&arr1, &arr2);
-
-    Display(*arr3);
+    Array *arr1;
+    int ch, sz;
+    int x, index;
+    cout << "Enter Size of Array:";
+    cin >> sz;
+    arr1 = new Array(sz);
+    do
+    {
+        cout << "\n\nMenu\n";
+        cout << "1. Insert\n";
+        cout << "2. Delete\n";
+        cout << "3. Search\n";
+        cout << "4. Sum\n";
+        cout << "5. Display\n";
+        cout << "6.Exit\n";
+        cout << "Enter you choice:";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+            cout << "Enter an element and index:";
+            cin >> x >> index;
+            arr1->Insert(index, x);
+            break;
+        case 2:
+            cout << "Enter index:";
+            cin >> index;
+            x = arr1->Delete(index);
+            cout << "Deleted Element is:\n"
+                 << x;
+            break;
+        case 3:
+            cout << "Enter element to search:";
+            cin >> x;
+            index = arr1->LinearSearch(x);
+            cout << "Element index:" << index;
+            break;
+        case 4:
+            cout << "Sum is\n"
+                 << arr1->Sum();
+            break;
+        case 5:
+            arr1->Display();
+        }
+    } while (ch < 6);
     return 0;
 }
